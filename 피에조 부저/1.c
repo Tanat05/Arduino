e6506063df44b2708ee1de732c459f0e396534bd
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-11-03 15:00:15 +0900

    Update 1.c

--- 피에조 부저/1.c
+++ 피에조 부저/1.c
@@ -1,1 +1,14 @@
+void setup() {
+  pinMode(5, OUTPUT);
+}
 
+void loop(){
+  tone(5, 261.6);
+  delay(200); noTone(5); delay(200);
+  tone(5, 293.7);
+  delay(200); noTone(5); delay(200);
+  tone(5, 329.6);
+  delay(200); noTone(5); delay(200);
+  tone(5, 349.2);
+  delay(200); noTone(5); delay(200);
+}
