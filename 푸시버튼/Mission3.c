604a0f9afdff7d7393873589d9cf703334b87ecf
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-11-08 10:28:52 +0900

    Create Mission3.c

---
+++ 푸시버튼/Mission3.c
@@ -0,0 +1,79 @@
+int button=0;
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+}
+
+void loop(){
+  if (digitalRead(2)==HIGH){
+    button=1;
+    delay(100);
+  }
+  else if (digitalRead(3)==HIGH){
+    button=2;
+    delay(100);
+  }
+  else if (digitalRead(4)==HIGH){
+    button=3;
+    delay(100);
+  }
+  
+  if(button==1){
+    digitalWrite(8, HIGH);
+    digitalWrite(9, HIGH);
+    digitalWrite(10, HIGH);
+    delay(300);
+    digitalWrite(8, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(10, LOW);
+    delay(300);
+  }
+  else if(button==2){
+    digitalWrite(8, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(10, HIGH);
+    delay(300);
+    digitalWrite(8, LOW);
+    digitalWrite(9, HIGH);
+    digitalWrite(10, LOW);
+    delay(300);
+    digitalWrite(8, HIGH);
+    digitalWrite(9, LOW);
+    digitalWrite(10, LOW);
+    delay(300);
+  }
+  else if(button==3){
+    digitalWrite(8, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(10, HIGH);
+    delay(300);
+    digitalWrite(8, LOW);
+    digitalWrite(9, HIGH);
+    digitalWrite(10, HIGH);
+    delay(300);
+    digitalWrite(8, HIGH);
+    digitalWrite(9, HIGH);
+    digitalWrite(10, HIGH);
+    delay(300);
+    digitalWrite(8, HIGH);
+    digitalWrite(9, HIGH);
+    digitalWrite(10, LOW);
+    delay(300);
+    digitalWrite(8, HIGH);
+    digitalWrite(9, LOW);
+    digitalWrite(10, LOW);
+    delay(300);
+    digitalWrite(8, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(10, LOW);
+    delay(300);
+  }
+}
