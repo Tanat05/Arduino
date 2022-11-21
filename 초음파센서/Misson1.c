eca0839c5068eee43d53534726037efe7d29fae4
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-11-21 11:45:19 +0900

    Update Misson1.c

--- 초음파센서/Misson1.c
+++ 초음파센서/Misson1.c
@@ -1,10 +1,10 @@
-int trig = 2;
-int echo = 3;
+int trig = 10;
+int echo = 11;
 void setup() {
   Serial.begin(9600);
-  pinMode(2, OUTPUT);
-  pinMode(3, INPUT);
-  pinMode(8, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(echo, INPUT);
+  pinMode(trig, OUTPUT);
 }
 
 void loop() {
@@ -13,13 +13,13 @@ void loop() {
   digitalWrite(trig, LOW);
   int distance = pulseIn(echo, HIGH) * 340/2/10000;
   if (distance<10){
-  	tone(8, 523.3, 100);
+    tone(3, 523.3, 100);
     delay(500);
   }
   else if (distance<20){
-  	tone(8, 392, 300);
+    tone(3, 392, 300);
     delay(1000);
   }
   else
-    noTone(8);
+    noTone(3);
 }
