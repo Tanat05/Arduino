32d616ac9c677edfbd4b69ddd5c90b9da3520742
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-12-01 15:21:52 +0900

    Update 완성본.c

--- 수행/프로젝트/완성본.c
+++ 수행/프로젝트/완성본.c
@@ -1,50 +1,41 @@
-// Set the pin for reading humidity from the temperature and humidity sensor
-int humiditySensorPin = A0;
-
-// Set the pin for reading soil moisture from the soil moisture sensor
-int soilMoistureSensorPin = A1;
-
-// Variable for storing the humidity data read from the temperature and humidity sensor
+// 데이터를 저장할 변수 선언
+int temperatureData;
 int humidityData;
-
-// Variable for storing the soil moisture data read from the soil moisture sensor
 int soilMoistureData;
 
-// Set the pin for controlling the humidifier
+// 센서의 핀과 제어 장치의 핀을 설정
+int temperatureSensorPin = A0;
+int humiditySensorPin = A1;
+int soilMoistureSensorPin = A2;
 int humidifierPin = 9;
-
-// Set the pin for controlling the pump
 int pumpPin = 10;
 
 void setup() {
-  // Set the humidifier control pin as an output
+  // 제어 장치의 핀을 출력으로 설정
   pinMode(humidifierPin, OUTPUT);
-
-  // Set the pump control pin as an output
   pinMode(pumpPin, OUTPUT);
 }
 
 void loop() {
-  // Read humidity data from the temperature and humidity sensor
+  // 센서로부터 데이터 읽기
+  temperatureData = analogRead(temperatureSensorPin);
   humidityData = analogRead(humiditySensorPin);
-
-  // Read soil moisture data from the soil moisture sensor
   soilMoistureData = analogRead(soilMoistureSensorPin);
 
-  // If the humidity is low, turn on the humidifier
+  // 습도가 낮을 경우 가습기 켜기
   if (humidityData < 30) {
     digitalWrite(humidifierPin, HIGH);
   }
-  // If the humidity is high, turn off the humidifier
+  // 습도가 높을 경우 가습기 끄기
   else if (humidityData > 50) {
     digitalWrite(humidifierPin, LOW);
   }
 
-  // If the soil moisture is low, turn on the pump
+  // 토양 수분이 낮을 경우 펌프 켜기
   if (soilMoistureData < 30) {
     digitalWrite(pumpPin, HIGH);
   }
-  // If the soil moisture is high, turn off the pump
+  // 토양 수분이 높을 경우 펌프 끄기
   else if (soilMoistureData > 50) {
     digitalWrite(pumpPin, LOW);
   }
