d4ec79ccc3515c324df361c8956a0b7fe116ecce
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-12-05 11:40:26 +0900

    Update 완성본.c

--- 수행/프로젝트/완성본.c
+++ 수행/프로젝트/완성본.c
@@ -1,45 +1,6 @@
-// 데이터를 저장할 변수 선언
-int temperatureData;
-int humidityData;
-int soilMoistureData;
-
-// 센서의 핀과 제어 장치의 핀을 설정
-int temperatureSensorPin = A0;
-int humiditySensorPin = A1;
-int soilMoistureSensorPin = A2;
-int humidifierPin = 9;
-int pumpPin = 10;
-
-void setup() {
-  // 제어 장치의 핀을 출력으로 설정
-  pinMode(humidifierPin, OUTPUT);
-  pinMode(pumpPin, OUTPUT);
-}
-
-void loop() {
-  // 센서로부터 데이터 읽기
-  temperatureData = analogRead(temperatureSensorPin);
-  humidityData = analogRead(humiditySensorPin);
-  soilMoistureData = analogRead(soilMoistureSensorPin);
-
-  // 습도가 낮을 경우 가습기 켜기
-  if (humidityData < 30) {
-    digitalWrite(humidifierPin, HIGH);
-  }
-  // 습도가 높을 경우 가습기 끄기
-  else if (humidityData > 50) {
-    digitalWrite(humidifierPin, LOW);
-  }
-
-  // 토양 수분이 낮을 경우 펌프 켜기
-  if (soilMoistureData < 30) {
-    digitalWrite(pumpPin, HIGH);
-  }
-  // 토양 수분이 높을 경우 펌프 끄기
-  else if (soilMoistureData > 50) {
-    digitalWrite(pumpPin, LOW);
-  }
-}
+#include <DHT11.h>
+int pin=2;    // 연결한 아두이노 디지털 핀 번호
+DHT11 dht11(pin);
 
 // 데이터를 저장할 변수 선언
 int humidityData;
@@ -51,6 +12,8 @@ int soilMoistureSensorPin = A1;
 int humidifierPin = 9;
 int pumpPin = 10;
 
+float temp, humi;
+
 void setup() {
   // 제어 장치의 핀을 출력으로 설정
   pinMode(humidifierPin, OUTPUT);
@@ -59,23 +22,37 @@ void setup() {
 }
 
 void loop() {
+  int err;
+  float temp, humi;
+  if((err=dht11.read(humi, temp))==0)
+  {
+    Serial.print("temperature:");
+    Serial.print(temp);
+    Serial.print(" humidity:");
+    Serial.print(humi);
+    Serial.println();
+  }
+  else
+  {
+    Serial.println();
+    Serial.print("Error No :");
+    Serial.print(err);
+    Serial.println();    
+  }
+
+  delay(DHT11_RETRY_DELAY);
   // 센서로부터 데이터 읽기
-  humidityData = analogRead(humiditySensorPin);
   soilMoistureData = analogRead(soilMoistureSensorPin);
 
-  Serial.println(humidityData);
-  Serial.println("1");
-  delay(1000);
+  Serial.print("soil: ");
   Serial.println(soilMoistureData);
-  Serial.println("2");
-  delay(1000);
   
   // 습도가 낮을 경우 가습기 켜기
-  if (humidityData < 30) {
+  if (humi < 30) {
     digitalWrite(humidifierPin, HIGH);
   }
   // 습도가 높을 경우 가습기 끄기
-  else if (humidityData > 50) {
+  else if (humi > 50) {
     digitalWrite(humidifierPin, LOW);
   }
 
